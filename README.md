1d4928cee1bd3f224ae5399b67371d7622ad6375
Mike Street <mikestreety@gmail.com>
2023-03-16 14:49:35 +0000

    Task: Set config to be more configurable
    Allow docs to be in the docs folder

--- README.md
+++ README.md
@@ -4,30 +4,46 @@ This is a framework around a framework. It is a quick, kick-start package allowi
 
 At Liquid Light we have our documentation located within the main site repo, so we made this to allow for lightweight deployment of our documentation framework. It comes with some (hopefully) sensible defaults.
 
+This can work as both a contained documentation framework within a site (e.g. if you have a `docs` folder in your repo) or as a documentation repo in itself.
+
 ## Setup
 
 - `npm init` if this is a brand new project
-- `npm i @liquidlight/docusaurus-framework`
+- `npm i @liquidlight/docusaurus-framework --save`
 - Create a `docusaurus.config.js` in the root of your project (or subfolder) with the following contents:
 
 ```js
-const config = require('@liquidlight/docusaurus-framework/docusaurus.config');
-
-module.exports = {
-	...config,
-	title: ''
-};
+module.exports = require('@liquidlight/docusaurus-framework/docusaurus.config')({
+	title: 'Liquid Light',
+});
 ```
-
 - Add the following scripts to the `package.json`
 
 ```json
 "scripts": {
-  "watch": "./node_modules/.bin/docusaurus start",
-  "build": "./node_modules/.bin/docusaurus build",
-  "serve": "./node_modules/.bin/docusaurus serve"
+  "watch": "docusaurus start",
+  "build": "docusaurus build",
+  "serve": "docusaurus serve"
 }
 ```
 
-- Create a `docs` folder, here you can add a series of folders and `.md` (or `.mdx` files) for you documentation content
+- Create a `docs` folder, here you can add a series of folders and `.md` (or `.mdx` files) for you documentation content - see below if your `package.json` is located inside your docs folder
 - Create a `static` folder - anything in here can be directly referenced in your Markdown files without the `static/` - e.g. `./static/img/logo.png` can be referenced as `img/logo.png`
+
+## Customisation
+
+A lot of the settings can be customised, along with allowing further expansion if required. All of the options are stored as defaults and can be overridden - check the top of `docusaurus.config.js`
+
+### Self-contained in the docs folder
+
+If you want your `package.json` file inside the `docs` folder so it is all self-contained, you need to set the following in your `docusaurus.config.js`
+
+```
+docs: {
+	path: './',
+}
+```
+
+### Remove the logo
+
+If you don't want the logo to appear in the nav bar, set `logo: {}` in the config object 
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,133 +6,156 @@
 const merge = require('deepmerge')
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-/**
-* Configuration
-*/
-const configOverrides = {
-	default_branch: 'main',
-	blog: false
-};
+module.exports = function (options = {}) {
+	/**
+	* Configuration
+	*/
+	const defaults = {
+		url: 'https://www.liquidlight.co.uk',
+		baseUrl: '/',
+		onBrokenLinks: 'warn',
+		onBrokenMarkdownLinks: 'warn',
+		favicon: 'img/favicon.ico',
+		organizationName: 'liquidlight', // Usually your GitHub org/user name.
+		projectName: 'documentation', // Usually your repo name.
+		tagline: '',
 
-/**
- * Configure based on Gitlab URL
- */
-let preset_docs = {
-	routeBasePath: '/',
-	sidebarPath: require.resolve('./sidebars.js'),
-};
+		blog: false,
 
-let navbar_items = [
-	{
-		type: 'doc',
-		docId: 'index',
-		position: 'left',
-		label: 'Documentation',
+		// To remove the logo, set `logo: {}`
+		logo: {
+			alt: options.title,
+			src: 'img/logo.svg',
+		},
+
+		footer: {
+			style: 'dark'
+		},
+
+		repository: {
+			branch: 'main',
+			host: 'Gitlab',
+			url: ''
+		},
+
+		docs: {
+			routeBasePath: '/',
+			sidebarPath: require.resolve('./sidebars.js'),
+			exclude: [
+				'node_modules',
+				'static',
+				'blog'
+			]
+		},
+
+		navbar_items: [
+			{
+				type: 'doc',
+				docId: 'index',
+				position: 'left',
+				label: 'Documentation',
+			}
+		],
+
+		prism: {
+			theme: darkCodeTheme,
+			darkTheme: darkCodeTheme,
+			additionalLanguages: ['php', 'apacheconf', 'typoscript'],
+		},
+
+		search: {
+			hashed: true,
+			indexBlog: false,
+			highlightSearchTermsOnTargetPage: true,
+			searchResultLimits: 12,
+			searchResultContextMaxLength: 75,
+			docsRouteBasePath: "/",
+			ignoreFiles: ['/archive/']
+		}
+	};
+
+	const configOverrides = merge(defaults, options);
+
+	if (configOverrides.blog) {
+		configOverrides.navbar_items.push({
+			to: 'blog',
+			label: 'Blog',
+			position: 'left'
+		});
+		configOverrides.search.indexBlog = true;
 	}
-];
 
-let hasBlog = false;
-if (configOverrides.blog) {
-	hasBlog = true;
-	navbar_items.push({
-		to: 'blog',
-		label: 'Blog',
-		position: 'left'
+	if (configOverrides.repository.url) {
+		preset_docs.editUrl = `${configOverrides.repository.url}${configOverrides.repository.host === 'Gitlab' ? '/-/tree/' : '/blob/'}${configOverrides.repository.branch}/`;
+
+		navbar_items.push({
+			href: configOverrides.gitlab_url,
+			label: configOverrides.repository.host,
+			position: 'right',
+		});
+
+		/**
+		* Delete custom properties
+		*/
+		delete configOverrides.gitlab_url;
 	}
-	);
-}
-delete configOverrides.blog;
-
-if (configOverrides.gitlab_url) {
-	preset_docs.editUrl = `${configOverrides.gitlab_url}/-/tree/${configOverrides.default_branch}/`;
-
-	navbar_items.push({
-		href: configOverrides.gitlab_url,
-		label: 'Gitlab',
-		position: 'right',
-	});
 
 	/**
-	* Delete custom properties
+	* Set Docusaurus defaults
 	*/
-	delete configOverrides.gitlab_url;
+	let config = {
+		presets: [
+			[
+				'classic',
+				/** @type {import('@docusaurus/preset-classic').Options} */
+				({
+					docs: configOverrides.docs,
+					blog: configOverrides.blog,
+					theme: {
+						customCss: require.resolve('./src/css/custom.css'),
+					},
+				}),
+			],
+		],
+
+		themeConfig:
+			/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+			({
+				navbar: {
+					title: configOverrides.title,
+					logo: configOverrides.logo,
+					items: configOverrides.navbar_items,
+				},
+				footer: configOverrides.footer,
+				prism: configOverrides.prism,
+			}),
+
+		themes: [
+			// ... Your other themes.
+			[
+				require.resolve("@easyops-cn/docusaurus-search-local"),
+				/** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+				(configOverrides.search),
+			],
+		],
+	};
+
+	/**
+	 * Delete non-standard configs
+	 */
+	delete configOverrides.blog;
+	delete configOverrides.docs;
+	delete configOverrides.footer;
+	delete configOverrides.logo;
+	delete configOverrides.navbar_items;
+	delete configOverrides.prism;
+	delete configOverrides.repository;
+	delete configOverrides.search;
+
+	/**
+	* Merge overrides with Docusaurus defaults
+	*/
+	config = merge(config, configOverrides);
+
+	return config;
 }
-
-delete configOverrides.default_branch;
-
-/**
-* Set Docusaurus defaults
-*/
-let config = {
-	url: 'https://www.liquidlight.co.uk',
-	baseUrl: '/',
-	onBrokenLinks: 'warn',
-	onBrokenMarkdownLinks: 'warn',
-	favicon: 'img/favicon.ico',
-	organizationName: 'liquidlight', // Usually your GitHub org/user name.
-	projectName: 'documentation', // Usually your repo name.
-	tagline: configOverrides.tagline ?? '',
-	presets: [
-		[
-			'classic',
-			/** @type {import('@docusaurus/preset-classic').Options} */
-			({
-				docs: preset_docs,
-				blog: hasBlog ? {} : false,
-				theme: {
-					customCss: require.resolve('./src/css/custom.css'),
-				},
-			}),
-		],
-	],
-
-	themeConfig:
-		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-		({
-			navbar: {
-				title: configOverrides.title,
-				logo: {
-					alt: 'Liquid Light',
-					src: 'img/logo.svg',
-				},
-				items: navbar_items,
-			},
-			footer: {
-				style: 'dark',
-			},
-			prism: {
-				theme: darkCodeTheme,
-				darkTheme: darkCodeTheme,
-				additionalLanguages: ['php', 'apacheconf', 'typoscript'],
-			},
-		}),
-
-	themes: [
-		// ... Your other themes.
-		[
-			require.resolve("@easyops-cn/docusaurus-search-local"),
-			/** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
-			({
-				// ... Your options.
-				// `hashed` is recommended as long-term-cache of index file is possible.
-				hashed: true,
-				indexBlog: hasBlog,
-				highlightSearchTermsOnTargetPage: true,
-				searchResultLimits: 12,
-				searchResultContextMaxLength: 75,
-				docsRouteBasePath: "/",
-				ignoreFiles: ['/archive/']
-			}),
-		],
-	],
-};
-
-/**
-* Merge overrides with Docusaurus defaults
-*/
-config = merge(config, configOverrides);
-
-/**
-* EXPORT
-*/
-module.exports = config;
